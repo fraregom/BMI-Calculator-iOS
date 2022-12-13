5b88f6327f7c7e2b0c505c14a5b60b9dbd924790
Francisco Reyes <francisco.reyesgom@gmail.com>
2022-12-13 00:12:52 -0300

    Getting and showing values from sliders

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        // Optional: String(format: "%.2f", sender.value)
+        let value = String(round(sender.value * 100) / 100.0)
+        heightLabel.text = "\(value)m"
+        print(value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let value = String(Int(sender.value))
+        weightLabel.text = "\(value)Kg"
+    }
+    
 
 }
 
